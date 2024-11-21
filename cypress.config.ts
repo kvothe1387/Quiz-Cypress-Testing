b5b2612844c0fd48acbe1efefcb66ad85f3ebcd4
David McCullough <dmccullough488@gmail.com>
2024-11-20 19:51:33 -0600

    docs: updated pkg.json & cypress.config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,16 +1,20 @@
 import { defineConfig } from "cypress";
+import customViteConfig from "./vite.config";
 
 export default defineConfig({
   component: {
     devServer: {
       framework: "react",
       bundler: "vite",
+      viteConfig: customViteConfig,
     },
+    specPattern: "cypress/**/**/*.cy.{js,jsx,ts,tsx}"
   },
 
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: "http://localhost:3000",
   },
 });
